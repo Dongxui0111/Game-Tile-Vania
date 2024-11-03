a28605c1a2e7f9d1b971c987f7b818b8ee94169b
Dongxui0111 <116093125+Dongxui0111@users.noreply.github.com>
2024-11-03 19:07:04 +0700

    scenes moi

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -8,10 +8,12 @@ public class LevelExit : MonoBehaviour
     [SerializeField] float levelLoadDelay = 2f;
     [SerializeField] float levelExitTimeFactor = 0.25f;
 
+    
     private void OnTriggerEnter2D(Collider2D collider)
     {
         StartCoroutine(LoadNextLevel());
     }
+
 
     IEnumerator LoadNextLevel()
     {
@@ -25,5 +27,8 @@ public class LevelExit : MonoBehaviour
 
         var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
+       
     }
 }
+
+
